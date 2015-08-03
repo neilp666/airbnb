a27569fd6b9e01df68bc2988d155ba9d2c1a1358
neil patel <neil@myindianweddingapp.com>
2015-08-03 11:30:32 +0100

    Added price & location information

--- config/initializers/airbnb.rb
+++ config/initializers/airbnb.rb
@@ -11,3 +11,11 @@ page = Nokogiri::HTML(open(url))
 page.css('h3.h5.listing-name').each do |line|
   puts line.text
 end
+
+page.css('span.h3.price-amount').each do |line|
+  puts line.text
+end
+
+page.css('div.text-muted.listing-location.text-truncate').each do |line|
+  puts line.text
+end
